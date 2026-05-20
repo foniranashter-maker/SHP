0b7f58221bd2c9309cd0fa8f56dd0ac842f5bf01
foniranashter-maker <foniranashter@gmail.com>
2026-05-20 14:23:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function login() {
         alert("Selamat datang " + found.nama);
 
         window.location.href =
-        "https://storage3.me-qr.com/pdf/d7b0fbf8-264f-48aa-b4a6-c1c92e3b1ca8";
+        "https://storage3.me-qr.com/pdf/7c0190fc-6c60-4fdd-b6fb-83541b528d70.pdf";
     } else {
         alert("User / password salah");
     }
